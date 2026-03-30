ff87a58f145dc5ee0f8a21f177ec61db62769631
mark beresford <berryreact@gmail.com>
2026-03-30 13:57:08 +1000

    tidy-icons

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,4 @@
 
-// import { login, logout } from "./login";
-
-// import { signUpUser } from "./userSettings";
-
-
-
 
 
 /// 		Lucide Icons		///
@@ -25,70 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
 //--------------------------- Functionality -----------------------------//
 
 
-
-///-------------------- User Login / logout / Signup --------------///
-
-// const loginBtn = document.querySelector('.login__form');
-// const logoutBtn = document.querySelector('.logout--btn');
-// const signupForm = document.querySelector('.signup__form');
-
-// const logoutAccountBtn = document.getElementById('my-account-logout');
-
-
-// if (loginBtn) {
-
-// 	loginBtn.addEventListener('submit', e => {
-
-// 		e.preventDefault();
-
-// 		const email = document.getElementById('login-email').value;
-// 		const password = document.getElementById('login-password').value;
-
-// 		login(email, password);
-// 	})
-// }
-
-
-// if (logoutBtn) {
-
-// 	logoutBtn.addEventListener('click', e => {
-
-// 		e.preventDefault();
-
-// 		logout();
-// 	})
-// }
-
-
-// if (logoutAccountBtn) {
-
-// 	logoutAccountBtn.addEventListener('click', e => {
-
-// 		e.preventDefault();
-
-// 		logout();
-// 	})
-// }
-
-
-// if (signupForm) {
-
-// 	signupForm.addEventListener('submit', e => {
-
-// 		e.preventDefault();
-
-// 		const data = {
-
-// 			name: document.getElementById('signup-name').value,
-// 			email: document.getElementById('signup-email').value,
-// 			password: document.getElementById('signup-password').value,
-// 			passwordConfirm: document.getElementById('signup-passwordConfirm').value,
-// 			phone: document.getElementById('signup-phone').value
-// 		}
-
-// 		signUpUser(data);
-// 	})
-// }
 
 
 //--------------------  hamburger  -------------------//
@@ -162,45 +92,5 @@ if (serviceGrid) {
 	});
 }
 
-
-
-
-
-//----------------- How it works bubbling  ---------------//
-
-
-
-// const howItWorks = document.getElementById("how-it-works");
-
-
-// if (howItWorks) {
-// 	const panels = howItWorks.querySelectorAll(".process__step-text");
-// 	const buttons = howItWorks.querySelectorAll(".process__step--btn");
-
-// 	howItWorks.addEventListener("click", (e) => {
-// 		const btn = e.target.closest(".process__step--btn");
-// 		if (!btn) return;
-
-// 		e.preventDefault();
-
-// 		const step = btn.dataset.step;
-
-// 		/// panels
-
-// 		panels.forEach((panel) => {
-// 			const isTarget = panel.classList.contains(`process-step--${step}`);
-// 			panel.classList.toggle("content-active", isTarget);
-// 			panel.classList.toggle("content-hidden", !isTarget);
-// 		});
-
-// 		/// buttons
-
-// 		buttons.forEach((b) => {
-// 			const isActive = b === btn;
-// 			b.classList.toggle("btn-active", isActive);
-// 			b.classList.toggle("btn-inactive", !isActive);
-// 		});
-// 	});
-// }
 
 
